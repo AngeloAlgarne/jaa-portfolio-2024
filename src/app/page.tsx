9740c6c07188d61121469cad6d64f3e87d7b6f3e
geloiam <74101193+AngeloAlgarne@users.noreply.github.com>
2023-12-17 17:09:44 +0000

    Update Hero and Nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,28 @@
 import { Hero, Navbar } from '@/components'
-import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex w-full min-h-screen flex-col items-center p-24">
       <Navbar />
-      <Hero />
+      <div className='w-4/5'>
+        <Hero />
+        <section>
+          <div className="flex flex-col gap-10 justify-center">
+            <h1>Hi,</h1>
+            <h1 className="text-8xl font-bold">Hello World.</h1>
+            <h1>Goodbye World.</h1>
+          </div>
+          <div className="flex flex-col justify-center">
+            <div className="overflow-hidden rounded-2xl w-fit">
+            </div>
+            <div className="flex justify-center gap-10 p-5">
+              <div>prev</div>
+              <div>pagination</div>
+              <div>next</div>
+            </div>
+          </div>
+        </section>
+      </div>
     </main>
   )
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className="flex-1 flex w-full h-full">
-      <div className="flex flex-col w-3/5 gap-10 justify-center">
-        <h1>Helloooo</h1>
+    <section className="flex h-full justify-between">
+      <div id="hero__left" className="flex flex-col gap-10 justify-center">
+        <h1>Hi,</h1>
         <h1 className="text-8xl font-bold">Hello World.</h1>
         <h1>Goodbye World.</h1>
       </div>
-      <div className="flex flex-col w-2/5 justify-center">
+      <div className="right-part flex flex-col justify-center">
         <div className="overflow-hidden rounded-2xl w-fit">
           <Image
             src="/img/my_image_1.jpg"
@@ -24,7 +24,7 @@ const Hero = () => {
           <div>next</div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
